a595fef701bbfcf725516ef145eb7b8db291ca86
MaryanGelle <>
2023-05-30 14:07:11 +0000

    changes

---
+++ assets/script.js
@@ -0,0 +1,73 @@
+
+
+
+// const question = {
+//     questionTex: "What is 2 + 2?",
+//     //Choices is an array of objects
+//     choices: [
+//         { text: '8', isCorrect: false },
+//         { text: '6', isCorrect: false },
+//         { text: '4', isCorrect: true },
+//         { text: '10', isCorrect: false },
+//     ]
+
+// }
+
+
+
+const questions = [
+    {
+        questionTex: "What is 2 + 2?",
+        choices: [
+            { text: '8', isCorrect: false },
+            { text: '6', isCorrect: false },
+            { text: '4', isCorrect: true },
+            { text: '10', isCorrect: false },
+        ]
+
+    },
+    {
+        questionTex: "What is the capital city of France?",
+        image: "images/paris.jpg",
+        choices: [
+            { text: 'London', isCorrect: false },
+            { text: 'Berlin', isCorrect: false },
+            { text: 'Paris', isCorrect: true },
+            { text: 'Rome', isCorrect: false },
+
+        ]
+
+
+
+    },
+    {
+        questionTex: "What is the capital city of Tounisia?",
+        image: ""
+        choises: [
+            { text: 'Agadir', isCorrect: false },
+            { text: 'Tounis', isCorrect: true },
+            { text: 'Leroun', isCorrect: false },
+            { text: 'London', isCorrect: false },
+        ]
+
+
+    },
+    {
+        questionTex: "Who wrote the play of Romeo and Juliet?",
+        choises: [
+            { text: 'Oscar Wild', isCorrect: false },
+            { text: 'James Austin', isCorrect: false },
+            { text: 'William Shakespear', isCorrect: true },
+            { text: 'Anna Rose', isCorrect: false },
+        ]
+
+
+    },
+
+
+
+];
+
+
+
+
