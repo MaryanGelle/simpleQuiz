8d249fe2d95ec517a0233fa1af75429fc7cbc823
MaryanGelle <>
2023-06-06 12:09:47 +0000

    changes

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -6,9 +6,10 @@ h1.innerText = `Welcome to the Quiz ${username}`;
 // Array have indexes
 // the  index starts from 0
 // The first element lies at index zero
+
 const questions = [
     {
-        questionTex: "What is 2 + 2?",
+        questionText: "What is 2 + 2?",
         choices: [
             { text: '8', isCorrect: false },
             { text: '6', isCorrect: false },
@@ -18,7 +19,7 @@ const questions = [
 
     },
     {
-        questionTex: "What is the capital city of France?",
+        questionText: "What is the capital city of France?",
         image: "images/paris.jpg",
         choices: [
             { text: 'London', isCorrect: false },
@@ -32,7 +33,7 @@ const questions = [
 
     },
     {
-        questionTex: "What is the capital city of Tounisia?",
+        questionText: "What is the capital city of Tounisia?",
         choices: [
             { text: 'Agadir', isCorrect: false },
             { text: 'Tounis', isCorrect: true },
@@ -43,7 +44,7 @@ const questions = [
 
     },
     {
-        questionTex: "Who wrote the play of Romeo and Juliet?",
+        questionText: "Who wrote the play of Romeo and Juliet?",
         choices: [
             { text: 'Oscar Wild', isCorrect: false },
             { text: 'James Austin', isCorrect: false },
@@ -54,8 +55,8 @@ const questions = [
 
     },
     {
-        questionTex: "Who painted the Mona Lisa?",
-        choises: [
+        questionText: "Who painted the Mona Lisa?",
+        choices: [
             { text: 'VINCENT VAN GOGH', isCorrect: false },
             { text: 'LÉONARD DE VINCI', isCorrect: true },
             { text: 'Pablo Picasso', isCorrect: false },
@@ -65,8 +66,8 @@ const questions = [
 
     },
     {
-        questionTex: "What is your body's largest organ",
-        choises: [
+        questionText: "What is your body's largest organ",
+        choices: [
             { text: 'Brain', isCorrect: false },
             { text: 'Heart', isCorrect: false },
             { text: 'Body', isCorrect: false },
@@ -80,18 +81,35 @@ const questions = [
 
 ];
 
-console.log(questions);
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
 
-// we are using camel case notaion to create the variable
-let currentQuestionIndex;
-
-
-
+let currentQuestionIndex = 0;
+let score = 0;
 
 
+function startQiuz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.questionText;
+
+    currentQuestion.choices.forEach(choice => {
+        const button = document.createElement("button");
+        button.innerHTML = choice.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+    });
+}
 
 
-
+startQuiz();
 
 
 
