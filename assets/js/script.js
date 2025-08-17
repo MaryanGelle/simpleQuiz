dc2035b355c1490f300a97b0840d4a7efa6a2739
maryangelle <maryan.gelle1@icloud.com>
2025-08-18 00:49:19 +0200

    changes to username

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,4 @@
 
-
-
-// const question = {
-//     questionTex: "What is 2 + 2?",
-//     //Choices is an array of objects
-//     choices: [
-//         { text: '8', isCorrect: false },
-//         { text: '6', isCorrect: false },
-//         { text: '4', isCorrect: true },
-//         { text: '10', isCorrect: false },
-//     ]
-
-// }
-
 const form = document.getElementById('username-form');
 const username = document.getElementById('username');
 
@@ -21,12 +7,6 @@ form.addEventListener('submit', (e) => {
     console.log('button clicked');
     console.log(username.value);
     console.log(window);
-    localStorage.setItem('username', JSON.stringify(username.value));
+    localStorage.setItem('username', username.value);
     window.location.href = 'quiz-page.html';
 });
-
-
-
-
-
-
